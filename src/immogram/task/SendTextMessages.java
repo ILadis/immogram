53f923e660c27315d7fe46396a34a6309fe81bd2
Michael Wagner <mwagn4@gmail.com>
2020-02-06 23:13:19 +0100

    Adds SendTextMessages task

---
+++ src/immogram/task/SendTextMessages.java
@@ -0,0 +1,28 @@
+package immogram.task;
+
+import java.util.Collection;
+import java.util.function.Supplier;
+
+import immogram.telegram.TelegramApi;
+import immogram.telegram.TextMessage;
+
+public class SendTextMessages implements Task<Collection<String>, Void> {
+
+	private final TelegramApi telegram;
+	private final Supplier<Integer> chatId;
+
+	public SendTextMessages(TelegramApi telegram, Supplier<Integer> chatId) {
+		this.telegram = telegram;
+		this.chatId = chatId;
+	}
+
+	@Override
+	public Void execute(Collection<String> input) {
+		for (var text : input) {
+			var message = new TextMessage(chatId.get(), 0, text);
+			telegram.sendTextMessage(message);
+		}
+		return null;
+	}
+
+}
