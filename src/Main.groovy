4b1aac1a5215286d82fe795698f15a9d0dacd167
Michael Wagner <mwagn4@gmail.com>
2020-02-06 23:39:27 +0100

    Adjusts groovy start script and maven build

--- src/Main.groovy
+++ src/Main.groovy
@@ -20,28 +20,26 @@ def builder = {
 }
 
 def bootstrap = builder()
+		.messagesLocale(Locale.GERMAN)
 		.jdbcUrl(props['JDBC_URL'])
 		.webDriverEndpoint('http://localhost:4444')
 		.telegramApiTokenAndEndpoint(props['BOT_TOKEN'], 'https://api.telegram.org')
 		.build()
 
-
-def immowelt = bootstrap.immoweltScraperTask()
-def ebay = bootstrap.ebayScraperTask()
-
-def tasks = [
-		immowelt.create("91413 Neustadt an der Aisch"),
-		immowelt.create("97346 Iphofen"),
-		ebay.create("91413 Neustadt an der Aisch"),
-		ebay.create("97346 Iphofen")]
-
-
 def bot = bootstrap.immogramBot()
 
-tasks.each { task ->
-	bot.tasks().add(task)
-	bot.tasks().setPeriod(Duration.ofHours(3))
+def taskRegistration = { factory, prefix ->
+	return { term -> bot.taskManager().register(prefix + " - " + term, factory.create(term)) }
 }
+
+def immowelt = taskRegistration(bootstrap.immoweltScraperTask(), 'Immowelt')
+def ebay = taskRegistration(bootstrap.ebayScraperTask(), 'Ebay')
+
+immowelt("91413 Neustadt an der Aisch")
+immowelt("97346 Iphofen")
+ebay("91413 Neustadt an der Aisch")
+ebay("97346 Iphofen")
+
 
 def process = 'geckodriver'.execute()
 process.consumeProcessOutput(System.out, System.err)
